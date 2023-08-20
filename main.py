e7dd97221fa050f13c6e388259f6785586dd222f
eeve1025 <126946790+eeve1025@users.noreply.github.com>
2023-08-20 14:42:17 +0000

    1

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+import streamlit as st
+
+def getStockNames()->pd.Series:
+        with open('codeSearch.csv',encoding='utf-8',newline='') as file:
+                next(file)
+                csv_reader = csv.reader(file)
+                stock_codes = {}
+                for item in csv_reader:
+                #print(item)
+                        key = item[2]
+                        stock_codes[key] = item[3]
+                #print(stock_codes)
+        code_series:pd.Series = pd.Series(stock_codes)
+        return code_series
+
+stockName:pd.Series = getStockNames()
+print(stockName)
+options = st.sidebar.multiselect('請選擇',
+                   stockNames.values,
+                   placeholder="股票:"  
+                       )
+print(options)
