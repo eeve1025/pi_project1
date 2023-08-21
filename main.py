bbdc0a1d7c25ddc48c4e63bf593856d5f440f0f3
eeve1025 <126946790+eeve1025@users.noreply.github.com>
2023-08-21 13:12:31 +0000

    1

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ def getStockNames()->pd.Series:
 stockName:pd.Series = getStockNames()
 print(stockName)
 
+def get_dataFrame(menu:list,start_year)->pd.DataFrame:
+    stock_data = ffn.get(menu,start=start_year)
+    return stock_data
+
+
 def displayData(dataFrame:pd.DataFrame,start_year) -> None:
         st.subheader(f'{start_year}~目前的歷史資料')
         st.dataframe(dataFrame)
@@ -39,4 +44,7 @@ prices = ffn.get(names, start='2010-01-01')
 
 if len(names) != 0:
     start_year = st.sidebar.selectbox("起始年份",range(2000,2023)) #起始年份選擇
+    dataFrame:pd.DataFrame= get_dataFrame(names,f"{start_year}-01-01")
+    #dataFrame1 = raname_columns_name(dataFrame,stockNames)
+    #st.sidebar.write("you selected:",start_year)
     displayData(prices,start_year=start_year)
